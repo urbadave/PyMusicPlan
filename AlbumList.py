a88b2d5af84058e958f8986fbc4c3b402b048009
David Urbanek <durbanek@comcast.net>
2022-07-10 16:58:35 -0600

    Reading albums from the same source as C# project

--- AlbumList.py
+++ AlbumList.py
@@ -1,4 +1,5 @@
 from typing import NamedTuple
+import re
 
 class Album(NamedTuple):
     artist: str
@@ -16,26 +17,19 @@ class AlbumList:
     def addAlbum(self, album: Album):
         if(album not in self.MyList):
             self.MyList.append(album)
+    
+    def loadFromFile(self, filePath: str):
+        with open(filePath, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                sp = re.split("\t+", line)
+                #print(sp)
+                self.addAlbum(Album(sp[0], sp[1], int(sp[2]), int(sp[3])))
+
 
 alist = AlbumList()
-alist.addAlbum(Album("I Fight Dragons", "Kaboom!", 2011, 110))
-alist.addAlbum(Album("IAMDYNAMITE", "Supermegafantastic", 2012, 111))
-alist.addAlbum(Album("Imagine Dragons", "Night Visions", 2012, 112))
-alist.addAlbum(Album("Imagine Dragons", "Smoke + Mirrors", 2014, 113))
-alist.addAlbum(Album("Indigo Girls", "Rites of Passage", 1992, 115))
-alist.addAlbum(Album("Ingrid Michaelson", "Lights Out", 2014, 116))
-alist.addAlbum(Album("INXS", "Kick", 1987, 117))
-alist.addAlbum(Album("Iron & Wine", "Kiss Each Other Clean", 2011, 118))
-alist.addAlbum(Album("Israel Kamakawiwo'ole", "Facing Future", 1993, 119))
-alist.addAlbum(Album("Jet", "Get Born", 2003, 120))
-alist.addAlbum(Album("Joe Jackson", "Look Sharp!", 1979, 121))
-alist.addAlbum(Album("Jónsi", "Go", 2010, 122))
-alist.addAlbum(Album("Journey", "Greatest Hits", 2006, 123))
-alist.addAlbum(Album("Julian Casablancas", "Phrazes For The Young", 2009, 124))
-alist.addAlbum(Album("Kansas", "Point Of Know Return", 1977, 125))
-alist.addAlbum(Album("Kasabian", "Velociraptor!", 2011, 126))
-alist.addAlbum(Album("Kasabian", "West Ryder Pauper Lunatic Asylum", 2009, 127))
-alist.addAlbum(Album("Kate Bush", "Hounds of Love", 1985, 128))
-alist.addAlbum(Album("KT Tunstall", "Eye To The Telescope", 2006, 129))
-alist.addAlbum(Album("KT Tunstall", "Tiger Suit", 2010, 130))
+fp = r"C:\Users\urban\Documents\MusicAlbums\Music Albums - Albums.tsv"
+alist.loadFromFile(fp)
 print("There are", len(alist.MyList), "albums")
+for a in alist.MyList:
+    print(a)
