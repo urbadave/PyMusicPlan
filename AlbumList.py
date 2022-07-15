40c46deaffe570f11622a1338692169ea7dfaad9
David Urbanek <durbanek@comcast.net>
2022-07-15 10:48:06 -0600

    writing to file

--- AlbumList.py
+++ AlbumList.py
@@ -9,17 +9,22 @@ class Album(NamedTuple):
     index: int
 
 class AlbumList:
-    def __init__(self):
+    def __init__(self, path):
         self.MyList = []
         self.ArtistLen = 0
         self.TitleLen = 0
+        self.Path = path
     
     def addAlbum(self, album: Album):
         if(album not in self.MyList):
             self.MyList.append(album)
+
+    def createFullPath(self, fileName: str):
+        return self.Path+"\\"+fileName
     
-    def loadFromFile(self, filePath: str):
-        with open(filePath, 'r') as f:
+    def loadFromFile(self, fileName: str):
+        fullPath = self.createFullPath(fileName)
+        with open(fullPath, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             for line in lines:
                 sp = re.split("\t+", line)
@@ -32,22 +37,35 @@ class AlbumList:
                 self.addAlbum(Album(artist, title, int(year), int(index)))
 
     def prettyAlbum(self, album: Album):
-        return f'{str.ljust(album.artist, self.ArtistLen)} {str.ljust(album.title, self.TitleLen)} {album.year}'
+        return f'{album.artist.ljust(self.ArtistLen, " ")} {album.title.ljust(self.TitleLen, " ")} {album.year}'
     
-    def prettyList(self):
+    def prettyList(self, addNewline: bool = False):
         prettyLines = []
+        extra = ''
+        if(addNewline):
+            extra = '\n'
         for a in self.MyList:
-            prettyLines.append(self.prettyAlbum(a))
+            prettyLines.append(self.prettyAlbum(a)+extra)
         return prettyLines
 
+    def writeFile(self, fileName: str):
+        fullPath = self.createFullPath(fileName)
+        linesToWrite = self.prettyList(True)
+        with open(fullPath, 'w', encoding='utf-8') as f:
+            f.writelines(linesToWrite)
+        return linesToWrite
 
-alist = AlbumList()
-fp = r"C:\Users\urban\Documents\MusicAlbums\Music Albums - Albums.tsv"
+
+p = r"C:\Users\urban\Documents\MusicAlbums"
+alist = AlbumList(p)
+fp = r"Music Albums - Albums.tsv"
 alist.loadFromFile(fp)
 print("There are", len(alist.MyList), "albums")
 print("Artist length is", alist.ArtistLen)
 print("TItle length is", alist.TitleLen)
-pretty = alist.prettyList()
+#pretty = alist.prettyList()
+
+pretty = alist.writeFile("testFile.txt")
 
 for p in pretty:
     print(p)
